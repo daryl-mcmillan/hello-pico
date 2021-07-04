38031ce66129a6b60a4ee746d1283b89c16fdc62
Daryl McMillan <darylm+git@gmail.com>
2021-07-04 09:08:55 -0400

    pwm

--- main.cc
+++ main.cc
@@ -1,19 +1,35 @@
 #include "pico/stdlib.h"
-#include "pico/binary_info.h"
+#include "hardware/pwm.h"
 
 const uint LED_PIN = 25;
 
-int main() {
-
-    bi_decl(bi_program_description("First Blink"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+int blink() {
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (1) {
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
         sleep_ms(1000);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+	gpio_put(LED_PIN, 0);
+	sleep_ms(100);
+	gpio_put(LED_PIN,1);
+	sleep_ms(100);
     }
+}
+
+int blink2() {
+	gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+	uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
+	pwm_set_wrap(slice_num, 65535U);
+	pwm_set_gpio_level(LED_PIN, 10000);
+	pwm_set_enabled(slice_num, true);
+	pwm_set_clkdiv_int_frac(slice_num, 255, 0);
+	for(;;) {}
+}
+
+int main() {
+	blink2();
+	return 0;
 }
